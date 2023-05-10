8578d5764e575bb323173b10a48b1b6a52f1c74d
xabid98 <526473+gb96@users.noreply.github.com>
2023-05-10 11:29:06 +0200

    Function that plays a single round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -15,8 +15,36 @@
      playerSelection=sign
      playerSelection.toLowerCase()
 
-  
-    return playRound()
+    if (playerSelection==="rock"||playerSelection==="paper"||playerSelection==="scissors") {
+        
+        if(playerSelection==="rock"){
+            
+            if(computerSelection==="rock")return 'You have tied! Rock equals Rock'
+            else if(computerSelection==="paper") return "You Lose! Paper beats Rock"
+            else if(computerSelection==="scissors") return "You Win! Rock beats scissors"
+
+        }
+        else if(playerSelection==="paper"){
+            
+            if(computerSelection==="paper")return 'You have tied! Paper equals paper'
+            else if(computerSelection==="scissors") return "You Lose! Scissors beats paper"
+            else if(computerSelection==="rock") return "You Win! Paper beats rock"
+
+        }
+        else if(playerSelection==="scissors"){
+
+            if(computerSelection==="scissors")return 'You have tied! Scissors equals scissors'
+            else if(computerSelection==="rock") return "You Lose! Rock beats scissors"
+            else if(computerSelection==="paper") return "You Win! Scissors beats paper"
+            
+
+        }
+    
+    
+    
+    }
+
+    else { return playRound()}
 
 
 
